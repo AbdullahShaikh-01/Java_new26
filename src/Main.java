38c3f92a4dce3da45c07abc0f2a5490ae8e50e3a
ABDULLAH BABU <mdabdullahbabu94@gmail.com>
2026-06-26 18:40:38 +0530

    First Java Program

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+public class Main{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 29;
+        int c = 20;
+        int sum = a+b+c;
+        System.out.println(sum);
+    }
+}
