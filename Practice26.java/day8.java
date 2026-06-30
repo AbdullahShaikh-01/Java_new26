ad3ef1c24ad47e21f288931283fc4ec1eacad61f
ABDULLAH BABU <mdabdullahbabu94@gmail.com>
2026-07-01 02:48:32 +0530

    Yes I want to commit this code!

---
+++ Practice26.java/day8.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class day8 {
+    public static void main(String[] args){
+
+//        // while loops
+//
+//        int a = 0;
+//        while (a<=10){
+//            System.out.println(a);
+//            a++;
+//        }
+    //do while loops
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter natural no: ");
+//        int natural_no,i;
+//        natural_no = sc.nextInt();
+//
+//       i = 1;
+//        do{
+//            System.out.println(i);
+//            i++;
+//        }while (i<=natural_no);
+//
+        // for loops
+//
+//      int n;
+//      System.out.print("enter number: ");
+//      Scanner sc = new Scanner(System.in);
+//      n = sc.nextInt();
+//      for (int i = 0; i <= n; i++){
+//          if (i%2!=0){
+//              System.out.println(i);
+//
+//          }
+//      }
+//
+//
+//        int n;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter number: ");
+//        n = sc.nextInt();
+//        for (int i=n; i>=1; i--){
+//            System.out.println("The First n Natural no is: "+i);
+//        }
+
+        // Practice questions
+
+
+        for (int i=1; i<=5; i++){
+            for (int j = 1; j<=i; j++){
+
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
