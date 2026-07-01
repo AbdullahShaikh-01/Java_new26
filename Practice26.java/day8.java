c48da18625e6a5dcb61366509e646eb972b8f402
ABDULLAH BABU <mdabdullahbabu94@gmail.com>
2026-07-01 17:28:55 +0530

    Yes I want to commit this code!

--- Practice26.java/day8.java
+++ Practice26.java/day8.java
@@ -1,63 +1,114 @@
+import com.sun.source.doctree.EscapeTree;
+
 import java.util.Scanner;
 
 public class day8 {
     public static void main(String[] args){
 
-//        // while loops
+////        // while loops
+////
+////        int a = 0;
+////        while (a<=10){
+////            System.out.println(a);
+////            a++;
+////        }
+//    //do while loops
+////        Scanner sc = new Scanner(System.in);
+////        System.out.print("Enter natural no: ");
+////        int natural_no,i;
+////        natural_no = sc.nextInt();
+////
+////       i = 1;
+////        do{
+////            System.out.println(i);
+////            i++;
+////        }while (i<=natural_no);
+////
+//        // for loops
+////
+////      int n;
+////      System.out.print("enter number: ");
+////      Scanner sc = new Scanner(System.in);
+////      n = sc.nextInt();
+////      for (int i = 0; i <= n; i++){
+////          if (i%2!=0){
+////              System.out.println(i);
+////
+////          }
+////      }
+////
+////
+////        int n;
+////        Scanner sc = new Scanner(System.in);
+////        System.out.print("Enter number: ");
+////        n = sc.nextInt();
+////        for (int i=n; i>=1; i--){
+////            System.out.println("The First n Natural no is: "+i);
+////        }
 //
-//        int a = 0;
-//        while (a<=10){
-//            System.out.println(a);
-//            a++;
-//        }
-    //do while loops
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter natural no: ");
-//        int natural_no,i;
-//        natural_no = sc.nextInt();
-//
-//       i = 1;
-//        do{
-//            System.out.println(i);
-//            i++;
-//        }while (i<=natural_no);
-//
-        // for loops
-//
-//      int n;
-//      System.out.print("enter number: ");
-//      Scanner sc = new Scanner(System.in);
-//      n = sc.nextInt();
-//      for (int i = 0; i <= n; i++){
-//          if (i%2!=0){
-//              System.out.println(i);
-//
-//          }
-//      }
+//        // Practice questions
 //
 //
-//        int n;
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter number: ");
+////        for (int i=1; i<=5; i++){
+////            for (int j = 1; j<=i; j++){
+////
+////                System.out.print("*");
+////
+////            }
+////            System.out.println();
+////        }
+////        for (int i=4; i>=0; i--){
+////            for (int j=0; j<=i; j++){
+////                System.out.print("*");
+////            }
+////            System.out.println();
+////        }
+
+//        int n,i,sum;
+//         Scanner sc = new Scanner(System.in);
+//        System.out.print("enter no: ");
 //        n = sc.nextInt();
-//        for (int i=n; i>=1; i--){
-//            System.out.println("The First n Natural no is: "+i);
+//        sum=0;
+//        i = 1;
+//
+//        while (i <= n){
+//            if (i%2==0){
+//                System.out.println(i);
+//                sum+=i;
+//            }
+//            i++;
 //        }
+//            System.out.println("total sum of the even no is: "+sum);
+//
+//        sc.close();
+//
+//
+//        int n,fact;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("enter no: ");
+//        n = sc.nextInt();
+//        fact = 1;
+//
+//        for (int i = 1; i <= n; i++){
+//            fact*=i;
+//            System.out.println(i);
+//        }
+//        System.out.println("fact is: "+fact);
 
-        // Practice questions
 
+                Scanner sc = new Scanner(System.in);
 
-        for (int i=1; i<=5; i++){
-            for (int j = 1; j<=i; j++){
+                System.out.print("Enter number: ");
+                int n = sc.nextInt();
+                int sum = 0;
 
-                System.out.print("*");
+                for (int i = 1; i <= 10; i++) {
+                    System.out.println(n + " x " + i + " = " + (n * i));
+                    sum+=(n*i);
+                }
 
-            }
-            System.out.println();
-        }
+        System.out.println("total sum of table: "+sum);
 
-
-
-
+                sc.close();
     }
 }
