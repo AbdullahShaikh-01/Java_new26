5237263eb8ec2c7377221585784e0f8ab135756c
ABDULLAH BABU <mdabdullahbabu94@gmail.com>
2026-07-19 23:26:43 +0530

    Yes I want to push this code!

---
+++ Practice26.java/day12_OOPs.java
@@ -0,0 +1,357 @@
+//class Student {
+//    String name;
+//    int age;
+//    int salary;
+//
+//    public void study() {
+//        System.out.println(name + " this is my name");
+//        System.out.println("my age is " +age);
+//    }
+//    public void printSalary(){
+//        System.out.println("my salary is " +salary);
+//    }
+//
+//}
+//public class day12_OOPs{
+//    public static void main(String[] args){
+//        Student s1 = new Student();
+//        Student s2 = new Student();
+//
+//        s1.name = "Abdullah";
+//        s1.age = 20;
+//        s1.salary = 23;
+//
+//        s2.name = "Babu";
+//        s2.age = 19;
+//        s2.salary = 34;
+//
+//        s1.study();
+//        s1.printSalary();
+//        System.out.println();
+//        s2.study();
+//        s2.printSalary();
+//
+//    }
+//}
+
+//class Square{
+//    int side;
+//
+//    public void setside(int n){
+//        side=n;
+//
+//    }
+//    public int area(){
+//        return side*side;
+//    }
+//    public int perimeter(){
+//        return 4*side;
+//    }
+//    public static void main(String[] args){
+//
+//    Square s1 = new Square();
+//    s1.setside(8);
+//        System.out.println("Area = "+s1.area());
+//        System.out.println("Perimeter = "+s1.perimeter());
+//
+//    }
+//}
+
+//class Cellphone{
+//    public void printRinging(){
+//        System.out.println("ringing");
+//    }
+//    public void printVibrating(){
+//        System.out.println("vibrating");
+//    }
+//
+//    public static void main(String[] args){
+//        Cellphone s1 = new Cellphone();
+//        s1.printRinging();
+//        s1.printVibrating();
+//    }
+//}
+
+
+
+//class Rectangle{
+//   int length;
+//   int breadth;
+//
+//
+//    public void setSides(int a, int b){
+//        length=a;
+//        breadth=b;
+//
+//    }
+//    public int area(){
+//        return length*breadth;
+//    }
+//    public int perimeter(){
+//        return 2*(length+breadth);
+//    }
+//    public static void main(String[] args){
+//
+//    Rectangle s1 = new Rectangle();
+//    s1.setSides(8, 6);
+//        System.out.println("Area = "+s1.area());
+//        System.out.println("Perimeter = "+s1.perimeter());
+//
+//    }
+//}
+
+
+//class MyEmployee{
+//    private int id;
+//    private String name;
+//    private int salary;
+//
+//    public void setId(int n){
+//        this.id=n;
+//
+//    }
+//    public int  getId(){
+//
+//        return id;
+//    }
+//
+//    public void setName(String  i){
+//        this.name=i;
+//
+//    }
+//    public String  getName(){
+//
+//        return name;
+//    }
+//    public void setSalary(int j){
+//        salary=j;
+//
+//    }
+//    public int  getSalary(){
+//
+//        return salary;
+//    }
+//
+//
+//    public static void main(String[] args){
+//        MyEmployee s1 = new MyEmployee();
+//        s1.setId(12);
+//        System.out.println(s1.getId());
+//
+//        s1.setName("Abdullah");
+//        System.out.println(s1.getName());
+//
+//        s1.setSalary(15000);
+//        System.out.println(s1.getSalary());
+//
+//
+//    }
+//
+//
+//}
+
+//
+//class Circle{
+//    private int radius;
+//    private double area;
+//    private double parimeter;
+//
+//    public void setRadius(int n){
+//        if(n>=0){
+//
+//        this.radius=n;
+//        }else{
+//            System.out.println("Radius cannot be negative.");
+//        }
+//    }
+//    public int getRadius(){
+//        return radius;
+//    }
+//
+//    public void setArea(double area){
+//        double actualArea = Math.PI*radius*radius;
+//        if(Math.abs(area-actualArea)<0.01){
+//            this.area=area;
+//        }else{
+//            System.out.println("Wrong Area");
+//        }
+//
+//
+//    }
+//
+//
+//    public double getArea(){
+//        return area;
+//    }
+//
+//    public void setParimeter(double perimeter){
+//        double actualPerimeter = 2*Math.PI*radius;
+//        if(Math.abs(perimeter - actualPerimeter) < 0.01){
+//            this.parimeter=perimeter;
+//        }else{
+//            System.out.println("Wrong Perimeter");
+//        }
+//    }
+//
+//    public double getParimeter(){
+//        return parimeter;
+//    }
+//
+//
+//
+//
+//
+//    public static void main(String[] args){
+//        Circle a1 = new Circle();
+//
+//        a1.setRadius(7);
+//
+//        a1.setArea(153.93804002589985);
+//        System.out.println(a1.getArea());
+//
+//
+//    }
+//
+//}
+
+//class Employee{
+//    String name;
+//    int salary;
+//
+//    Employee(String n, int a){
+//        name=n;
+//        salary=a;
+//    }
+//    public static void main(String[] args){
+//
+//    Employee s1= new Employee("ABDULLAH", 20000);
+//
+//        System.out.println(s1.name);
+//        System.out.println(s1.salary);
+//    }
+//}
+
+//class Student{
+//    String name;
+//    int age;
+//    String course;
+//
+//    Student(String name, int age, String course){
+//        this.name=name;
+//        this.age=age;
+//        this.course=course;
+//
+//
+//    }
+//
+//    public void display(){
+//        System.out.println("Name: "+name);
+//        System.out.println("Age: "+age);
+//        System.out.println("Course: "+course);
+//    }
+//
+//    public static void main(String[] args){
+//        Student s2 = new Student("Abdullah", 20, "B Tech CSE");
+//        Student s3 = new Student("Saziya", 19, "M Tech");
+//
+//        s2.display();
+//        System.out.println();
+//        s3.display();
+//    }
+//
+//}
+//
+//class MainEmpoyee{
+//    int salary;
+//
+//    public MainEmpoyee(int salary){
+//        this.salary = salary;
+//    }
+//
+//    public MainEmpoyee(){
+//        this.salary=25000;
+//    }
+//
+//    public static void main(String[] args){
+//        MainEmpoyee d1 = new MainEmpoyee(10000);
+//        MainEmpoyee d2 = new MainEmpoyee();
+//        System.out.println(d1.salary);
+//        System.out.println(d2.salary);
+//    }
+//}
+
+//import java.util.Scanner;
+//
+//public class day12_OOPs {
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Guess the number: ");
+//        int target = 50;
+//        int num=sc.nextInt();
+//
+//        while (num!=target){
+//            if (num<target){
+//                System.out.println("Too Low");
+//            } else{
+//                System.out.println("Too High");
+//
+//            }
+//            System.out.println("Sorry Bro, Guess Again");
+//            num = sc.nextInt();
+//        }
+//        System.out.println("Congratulation Bro, You Are Guess Right Number");
+//
+//
+//
+//
+//
+//        }
+//
+//    }
+
+import java.util.Scanner;
+
+class GuessGame {
+    int target;
+
+    GuessGame(int target) {
+        this.target = target;
+    }
+
+    void playGame() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Guess a number: ");
+        int num = sc.nextInt();
+
+        while (num != target) {
+            if (Math.abs(target - num)<=5){
+                System.out.println("Very Close!");
+            } else if (num<target){
+                System.out.println("Too Low!");
+            }else{
+                System.out.println("Too High!");
+            }
+
+            System.out.println("Oops Guess Again !");
+            num = sc.nextInt();
+
+        }
+        System.out.println("Congratulations Bro !");
+        sc.close();
+
+
+    }
+
+    public static void main(String[] args) {
+        GuessGame game = new GuessGame(50);
+        game.playGame();
+
+    }
+
+}
+
+
+
+
+
+
